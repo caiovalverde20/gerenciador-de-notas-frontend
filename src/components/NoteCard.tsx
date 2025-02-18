058cc6a3a82c8be07cff6d5fe2bab084bc49381b
caiovalverde20 <caiovalverde20@gmail.com>
2025-02-18 18:25:18 -0300

    Adicionado mais opçoes de cores para notas

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -7,12 +7,18 @@ interface NoteCardProps {
 }
 
 const COLORS = [
-  "bg-yellow-200",
-  "bg-orange-200",
-  "bg-green-200",
-  "bg-purple-200",
-  "bg-blue-200",
-];
+    "bg-yellow-200",
+    "bg-orange-200",
+    "bg-green-200",
+    "bg-purple-200",
+    "bg-blue-200",
+    "bg-pink-200",
+    "bg-red-200",
+    "bg-indigo-200",
+    "bg-teal-200",
+    "bg-lime-200",
+  ];
+  
 
 export default function NoteCard({ note, onNoteUpdate }: NoteCardProps) {
   const [isEditing, setIsEditing] = useState(false);
